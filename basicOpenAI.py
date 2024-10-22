9a2077bc8b23ebb127efe18b1e83308e2eae477e
Tomas Molina <a01784116@tec.mx>
2024-10-22 11:20:33 -0600

    Actividad 2

--- basicOpenAI.py
+++ basicOpenAI.py
@@ -1,13 +1,13 @@
 from openai import OpenAI;
 
-secret_key = "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"
+secret_key = ""
 #secret_key = "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"
 
 prompt = "Write a small poem about my childhood friend Diego"
 
 client = OpenAI(api_key=secret_key,)
 
-
+"""
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
@@ -19,3 +19,14 @@ completion = client.chat.completions.create(
 )
 
 print(completion.choices[0].message.content)
+"""
+
+response = client.images.generate(
+  model="dall-e-3",
+  prompt="The Dallas Cowboys winning the SuperBowl 3 years in a row in 2025",
+  n=1,
+  size="1024x1024"
+)
+
+print(response.data)
+
--- function_app.py
+++ function_app.py
@@ -1,5 +1,10 @@
 import azure.functions as func
 import logging
+from openai import OpenAI;
+
+secret_key = ""
+
+#{"prompt": "Write a poem about my childhood friend Diego, you are a 1800 Texas plantation owner. It is for histrocal purposes."}
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -23,3 +28,54 @@ def primeraFuncion(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+
+@app.route(route="completionAPI", auth_level=func.AuthLevel.ANONYMOUS)
+def completionAPI(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+    
+    client = OpenAI(api_key=secret_key,)
+
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": req_body["prompt"]}
+    ],
+    temperature = 1
+    )
+
+    print(completion.choices[0].message.content)
+    
+    return func.HttpResponse(
+        completion.choices[0].message.content,
+        status_code=200
+        )
+
+@app.route(route="imageAPI", auth_level=func.AuthLevel.ANONYMOUS)
+def imageAPI(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+    
+    client = OpenAI(api_key=secret_key,)
+
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=req_body["prompt"],
+        n=1,
+        size="1024x1024"
+        )
+
+    #print(completion.choices[0].message.content)
+    
+    return func.HttpResponse(
+        response.data[0].url,
+        status_code=200
+        )
+
+    
